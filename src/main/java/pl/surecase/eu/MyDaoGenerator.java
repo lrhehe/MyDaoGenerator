9b568abba766562d9b3c229a8dba1d179799f3eb
lrhehe <lrhehe@gmail.com>
2015-06-10 00:13:57 +0800

    MOD: for ddup

--- src/main/java/pl/surecase/eu/MyDaoGenerator.java
+++ src/main/java/pl/surecase/eu/MyDaoGenerator.java
@@ -7,21 +7,33 @@ import de.greenrobot.daogenerator.Schema;
 public class MyDaoGenerator {
 
     public static void main(String args[]) throws Exception {
-        Schema schema = new Schema(3, "com.yuantiku.dbdata");
-        addAccount(schema);
+        Schema schema = new Schema(1, "com.ddup.dbdata");
+        Entity goal = addGoal(schema);
+        addGoalCategory(schema, goal);
         new DaoGenerator().generateAll(schema, args[0]);
     }
 
-    private static void addAccount(Schema schema) {
-        Entity account = schema.addEntity("Account");
-        account.implementsSerializable();
-        account.addStringProperty("name").notNull();
-        account.addStringProperty("ldap").notNull().primaryKey();
-        account.addStringProperty("email");
-        account.addStringProperty("phone");
-        account.addStringProperty("dept");
-        account.addStringProperty("googleAccount");
-        account.addStringProperty("birth");
-        account.addStringProperty("constellation");
+    private static Entity addGoal(Schema schema) {
+        Entity goal = schema.addEntity("Goal");
+        goal.setHasKeepSections(true);
+        goal.implementsSerializable();
+        goal.addIdProperty();
+        goal.addShortProperty("desc");
+        goal.addLongProperty("createTime");
+        goal.addStringProperty("status");
+        return goal;
+    }
+
+    private static void addGoalCategory(Schema schema, Entity goal) {
+        Entity goalCategory = schema.addEntity("GoalCategory");
+        goalCategory.setHasKeepSections(true);
+        goalCategory.implementsSerializable();
+        goalCategory.addIdProperty();
+        goalCategory.addShortProperty("desc");
+        goalCategory.addLongProperty("createTime");
+        goalCategory.addStringProperty("status");
+
+        goalCategory.addToMany(goal, goal.addLongProperty("categoryId").getProperty());
+
     }
 }
